15d90a838791f870ddf285f91bde51aa0fb7668a
slayingMoon <g.kolev15@gmail.com>
2022-05-20 17:03:43 +0300

    first tests

--- src/test/java/AxeTest.java
+++ src/test/java/AxeTest.java
@@ -1,8 +1,31 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AxeTest {
 
-    public void testWeaponLosesDurabilityAfterAttack() {
+    @Test
+    public void testAxeLosesDurabilityAfterAttack() {
+        //Arrange
+        Axe axe = new Axe(10, 10);
+        Dummy dummy = new Dummy(10, 10);
+        //Act
+        axe.attack(dummy);
+        //Assert
+        //expectations vs reality
+        Assert.assertEquals(9, axe.getDurabilityPoints());
+    }
 
+    //Assert
+    @Test(expected = IllegalStateException.class)
+    public void testAxeAttackThrowsExceptionIfAxeIsBroken() {
+        //Arrange
+        Axe brokenAxe = new Axe(10, 1);
+        Dummy dummy = new Dummy(10, 10);
+        //Act
+        brokenAxe.attack(dummy);
+        //should break
+        brokenAxe.attack(dummy);
     }
 }
